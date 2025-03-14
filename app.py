e8f628f4a1116ea9dbc4ad1ae3e3889bd4aaca70
antoinedebonna <antdebonna@gmail.com>
2025-03-14 02:42:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -65,6 +65,7 @@ if not filtered_data.empty:
     cumulative_wins = filtered_data.copy()
     cumulative_wins["Victoire_Cumul"] = cumulative_wins.groupby("Joueur")["Résultat"].transform(lambda x: (x == "✅ V").astype(int).cumsum())
 
+
     
     fig_line = px.line(cumulative_wins, x="Match_Numero", y="Victoire_Cumul", color="Joueur", markers=True,
                         title="Évolution des victoires par joueur",
